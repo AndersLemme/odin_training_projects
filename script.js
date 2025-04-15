0adf63e8eb42864e94915027584e3afe0892615d
AndersLemme <a.lemme@hotmail.com>
2025-04-15 22:19:40 +0200

    Created blue h3 with javascript. (also used vim since vs code didnt work)

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ content.textContent = "This is the glorious text-content!";
 
 const red = document.createElement("p");
 red.classList.add("red");
-red.style.color = "red"
+red.style.color = "red";
 red.textContent = "Hey, i'm red!";
+
+const blue = document.createElement("h3");
+blue.classList.add("blue");
+blue.style.color = "blue";
+blue.textContent = "I'm a blue h3!";
 
 container.appendChild(content);
 container.appendChild(red);
+container.appendChild(blue);
 
